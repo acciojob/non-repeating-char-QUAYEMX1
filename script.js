be415aa2cb14ef1792af7a31c3de03fb05e9a7a7
QUAYEMX1 <126396459+QUAYEMX1@users.noreply.github.com>
2024-07-27 12:58:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	const arr={}
+	for(let i=0;i<str.length;i++){
+		if(arr[str[i]]){
+			arr[str[i]]++;
+		}
+	}
+	for(int i=0;i<arr.length;i++){
+		if(arr[i]==1){
+			return i;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
